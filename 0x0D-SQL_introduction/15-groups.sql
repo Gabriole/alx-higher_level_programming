d93fe6153feba44a55ec249d15e107032e1411fe
Gabriole <ismail.mouhtaram@gmail.com>
2024-05-21 05:54:00 -0700

    SQL

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+SELECT score, COUNT(1) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+Updates to keyboard shortcuts … On Thursday, August 1, 2024, Drive keyboard shortcuts will be updated to give you first-letters navigation.Learn more
+SELECT score, name
+FROM second_table
+HAVING name IS NOT NULL
+ORDER BY score DESC;
